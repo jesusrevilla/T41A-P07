469ba524ac4c9ce8ad0a9f6fd7fb23f059708618
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-07 19:42:01 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,10 @@
-
+---- Obtén todos los productos disponibles con información de marca (brand) en la columna metadata
+SELECT * FROM data_types_demo WHERE metadata ? 'brand';
+-- Encuentra los artículos lanzados después de una fecha específica
+SELECT * FROM data_types_demo WHERE launch_date > '2024-10-15';
+-- Busca por tag
+SELECT * FROM data_types_demo  WHERE 'demo' = ANY (tags);
+-- Muestra IP y dirección MAC
+SELECT  ip_address, mac_address FROM data_types_demo;
+-- Muestra coordenadas
+SELECT  coordinates  FROM data_types_demo;
