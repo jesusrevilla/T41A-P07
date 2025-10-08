94d4d97967270cf38660f7ed5a4a61decd544026
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-10-07 19:40:04 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,23 +1,24 @@
--- 1. Obtener todos los productos disponibles con información de marca (brand) en metadata
+-- 1. GET ALL AVAILABLE PRODUCTS WITH BRAND INFO IN METADATA
 SELECT *
 FROM data_types_demo
 WHERE available = TRUE
   AND metadata ? 'brand';
 
--- 2. Encontrar los artículos lanzados después de una fecha específica
+-- 2. FIND ITEMS LAUNCHED AFTER A SPECIFIC DATE
 SELECT *
 FROM data_types_demo
 WHERE launch_date > '2025-11-01';
 
--- 3. Buscar productos por un tag específico ('electronics' por ejemplo)
+-- 3. SEARCH PRODUCTS BY TAG 'electronics'
 SELECT *
 FROM data_types_demo
 WHERE 'electronics' = ANY(tags);
 
--- 4. Mostrar IP y dirección MAC de todos los productos
+-- 4. SHOW IP AND MAC ADDRESS
 SELECT name, ip_address, mac_address
 FROM data_types_demo;
 
--- 5. Mostrar las coordenadas de todos los productos
+-- 5. SHOW COORDINATES
 SELECT name, coordinates
 FROM data_types_demo;
+
