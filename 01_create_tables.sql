e0369327cb95d6b186ba234878c160b463b655ed
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-10-07 19:47:14 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -2,16 +2,16 @@ CREATE TABLE data_types_demo (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    price NUMERIC(5,2),
+    price NUMERIC(10,2),
     discount REAL,
     available BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT NOW(),
     launch_date DATE,
-    uuid_col UUID,
+    uuid_col UUID DEFAULT gen_random_uuid(),
     ip_address INET,
     mac_address MACADDR,
     tags TEXT[],
-    metadata JSONB,                 -- CAMBIO aquí
-    coordinates POINT,             -- CAMBIO aquí
+    metadata JSONB,
+    coordinates POINT,
     file_data BYTEA
 );
