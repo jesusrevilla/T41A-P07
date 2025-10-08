273fc636e138c796f90eeed5f0d70ac96a78ba0a
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-07 23:07:57 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -16,9 +16,4 @@ CREATE TABLE data_types_demo (
     file_data BYTEA NOT NULL
 );
 
-INSERT INTO data_types_demo (name, description, price, discount, available, launch_date, uuid_col, ip_address, mac_address, tags, metadata, coordinates, file_data) 
-  VALUES ('Jesus','Alberto', 2, 100, TRUE, '2025-05-05', 'a1b2c3d4-e5f6-7890-1234-567890abcdef', '127.0.0.1', 'BD-EA-44-F3-4C-98', ARRAY['1'], jsonb_build_object(
-        'color', 'rojo',
-        'talla', 'M',
-        'material', 'algodón'
-    ), POINT(-99.1332, 19.4326), '\x48656c6c6f20616c206d756e646f');
+
