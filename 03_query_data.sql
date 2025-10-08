0c1b397fed535f85265b9fe0f530dbab0e6e2c37
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-10-07 19:19:15 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,54 @@
+SELECT
+    id,
+    name,
+    price,
+    metadata->>'brand' AS brand_name,
+    metadata
+FROM
+    data_types_demo
+WHERE
+    available = TRUE
+    AND metadata ? 'brand';
 
+
+
+SELECT
+    id,
+    name,
+    launch_date
+FROM
+    data_types_demo
+WHERE
+    launch_date > '2024-05-01'
+ORDER BY
+    launch_date;
+
+
+
+SELECT
+    id,
+    name,
+    tags
+FROM
+    data_types_demo
+WHERE
+    tags @> ARRAY['electronics'];
+
+
+
+SELECT
+    id,
+    name,
+    ip_address,
+    mac_address
+FROM
+    data_types_demo;
+
+
+
+SELECT
+    id,
+    name,
+    coordinates
+FROM
+    data_types_demo;
