000f390b188096ce707751bb13c54312443f62fe
Cruz <183213@upslp.edu.mx>
2025-10-07 22:19:14 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO data_types_demo(name,description,price,discount,available,created_at,launch_date,uuid_col,ip_address,mac_address,tags,metadata,coordinates,file_data) VALUES
-('Entero','un numero completo','145.67','55.50',FALSE,NOW(),'2022-05-10','a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11','192.168.0.1/24','08-00-2b-01-02-03','{Entero}',
+('Entero','un numero completo','145.67','55.50',FALSE,NOW(),'2022-05-10','a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11','192.168.0.1','08-00-2b-01-02-03','{Entero}',
 '{"type": "entero", "brand": "001001"}','(5,4)','\123'),
-('Flotante','Un numero en decimal','225.12','44.40',TRUE,NOW(),'2023-06-17','a0eebc999c0b4ef8bb6d6bb9bd380a11','192.168.0.2/24','08-00-2b-01-02-05','{Flotante}',
+('Flotante','Un numero en decimal','225.12','44.40',TRUE,NOW(),'2023-06-17','a0eebc999c0b4ef8bb6d6bb9bd380a11','192.168.0.2','08-00-2b-01-02-05','{Flotante}',
 '{"type": "entero", "brand": "521481"}','(8,9)','\001');
