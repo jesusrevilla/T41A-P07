27fa49ff015bdd4e232abd1f4edef3b43552ef8f
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-10-07 19:26:18 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,19 @@
 
+INSERT INTO data_types_demo (
+    name, description, price, discount, available, launch_date, uuid_col,
+    ip_address, mac_address, tags, metadata, coordinates, file_data
+) VALUES (
+    'Producto Simple',
+    'Descripción básica.',
+    9.99,
+    0.0,
+    TRUE,
+    '2025-01-01',
+    gen_random_uuid(),
+    '127.0.0.1',
+    '00:00:00:00:00:00',
+    ARRAY['tag1', 'simple'],
+    '{"brand": "MiMarca"}',
+    POINT(0, 0),
+    DECODE('00', 'hex') 
+);
