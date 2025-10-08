74095a4f72a8eabcdba15c6cc03c4216d369c9a9
m00n07 <174653@upslp.edu.mx>
2025-10-07 19:48:51 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,14 +1,10 @@
--- Obtén todos los productos disponibles con información de marca (brand) en la columna metadata
-SELECT * FROM data_types_demo WHERE available = TRUE  AND metadata ? 'brand';
-
--- Encuentra los artículos lanzados después de una fecha específica
-SELECT * FROM data_types_demo WHERE launch_date > '2025-10-15';
-
--- Busca por tag
-SELECT * FROM data_types_demo WHERE 'promoción' = ANY(tags);
-
--- Muestra IP y dirección MAC
+-- 1
+SELECT name, price, metadata ->> 'brand' AS brand FROM data_types_demo WHERE available = TRUE AND metadata ? 'brand';
+-- 2
+SELECT name, launch_date FROM data_types_demo WHERE launch_date > '2026-01-01';
+-- 3 
+SELECT name, tags FROM data_types_demo WHERE 'oferta' = ANY(tags);
+-- 4   
 SELECT name, ip_address, mac_address FROM data_types_demo;
-
--- Muestra coordenadas
+-- 5
 SELECT name, coordinates FROM data_types_demo;
