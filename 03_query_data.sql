3593c686860d9abe45b7527a02979f7cea999680
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-07 19:46:00 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,9 +1,5 @@
 select * from data_types_demo where metadata ? 'brand';
-
 select * from data_types_demo where launch_date>'2025-09-01';
-
 select * from data_types_demo where 'gratis'=ANY(tags);
-
 select ip_address, mac_address from data_types_demo;
-
 select coordinates from data_types_demo;
