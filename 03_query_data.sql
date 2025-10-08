d8f78d046f6304ef1e24ce9a8c4b81e90d81795b
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-07 19:28:38 -0600

    feature/182483

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,9 @@
+SELECT name, price, metadata->>'brand' AS brand FROM data_types_demo WHERE available = TRUE AND metadata ? 'brand';
 
+SELECT name, launch_date FROM data_types_demo WHERE launch_date > '2024-12-31';
+
+SELECT name, tags FROM data_types_demo WHERE 'gaming' = ANY(tags);
+
+SELECT name, ip_address, mac_address FROM data_types_demo;
+
+SELECT name, coordinates FROM data_types_demo;
