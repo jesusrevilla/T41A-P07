7d61ae3f60467a2068b9917b757a3fb2217e25fe
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-07 19:47:05 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,18 +1,21 @@
+--Yovana Isabel Palencia Sánchez 174197
+
 CREATE TABLE data_types_demo (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     description TEXT,
-    price NUMERIC(10, 2),
-    discount REAL,
+    price NUMERIC(10,2),
+    discount FLOAT,
     available BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT NOW(),
     launch_date DATE,
-    uuid_col UUID,
+    uuid_col UUID DEFAULT gen_random_uuid(),
     ip_address INET,
     mac_address MACADDR,
     tags TEXT[],
-    metadata JSON,
-    coordinates TEXT,
+    metadata JSONB,
+    coordinates POINT,
     file_data BYTEA
 );
+
 
