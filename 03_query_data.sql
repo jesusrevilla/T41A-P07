b27cddbaf0144e71e7457fbf54b0bf525df9d063
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-07 19:49:56 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -2,28 +2,27 @@
 SELECT *
 FROM data_types_demo
 WHERE available = TRUE
-  AND metadata->>'brand' IS NOT NULL;
+  AND metadata ? 'brand';
 
 -- 2. Encuentra los artículos lanzados después de una fecha específica (ejemplo: 2025-01-01)
 SELECT *
 FROM data_types_demo
-WHERE launch_date > '2025-01-01';
+WHERE launch_date > '2025-07-01';
+
 
 -- 3. Busca productos que tengan un tag específico (ejemplo: 'portátil')
 SELECT *
 FROM data_types_demo
-WHERE 'portátil' = ANY(tags);
+WHERE 'tecnología' = ANY(tags);
+
 
 -- 4. Muestra IP y dirección MAC de todos los productos
-SELECT 
-    name, 
-    ip_address, 
-    mac_address
+SELECT name, ip_address, mac_address
 FROM data_types_demo;
 
+
 -- 5. Muestra nombre y coordenadas de todos los productos
-SELECT 
-    name, 
-    coordinates
+SELECT name, coordinates
 FROM data_types_demo;
+
 
