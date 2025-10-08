8663af6c9b38705def2104122e1966d226adaa9d
Diego0086 <178218@upslp.edu.mx>
2025-10-07 19:48:15 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,18 @@
+CREATE TABLE data_types_demo(
+    id SERIAL PRIMARY KEY,                       
+    name varchar(50) NOT NULL,                       
+    description text,                                
+    price numeric(10,2),                           
+    discount float,                                
+    available BOOLEAN DEFAULT true,                                  
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),                
+    launch_date date,                                
+    uuid_col uuid,                                   
+    ip_address inet,                                
+    mac_address macaddr,                             
+    tags text[],                                       
+    metadata jsonb,                                 
+    coordinates point,                             
+    file_data bytea                      
+);
 
