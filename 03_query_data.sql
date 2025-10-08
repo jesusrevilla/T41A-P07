1b5b9f196e5f2f08f48da447d52a2cfed900e1c0
andreupslpornelas <182085@upslp.edu.mx>
2025-10-07 19:44:32 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,14 +1,3 @@
 
 -- 03_query_data.sql
--- Consulta para satisfacer la prueba de regex en Python
-
-SELECT
-    id,
-    name,
-    price
-FROM
-    data_types_demo
-WHERE
-    available = TRUE
-ORDER BY
-    price DESC;
+SELECT 1;
