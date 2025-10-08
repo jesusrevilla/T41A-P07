18e25450e47919ea2e1b5583fc5317ca779c0020
menaxixi <179800@upslp.edu.mx>
2025-10-07 19:54:53 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,9 @@
+SELECT * FROM data_types_demo WHERE available = TRUE AND metadata ->> 'marca' IS NOT NULL;
 
+SELECT * FROM data_types_demo WHERE launch_date > '2025-10-06';
+
+SELECT * FROM data_types_demo WHERE 'Samsung' = ANY(tags);
+
+SELECT name, ip_address, mac_address FROM data_types_demo;
+
+SELECT name, coordinates FROM data_types_demo;
