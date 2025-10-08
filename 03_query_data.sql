76130a5e5fb03ab337e2d068c01dd45bee1e07d3
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-08 01:09:15 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,28 +1,16 @@
 -- 1. Obtén todos los productos disponibles con información de marca (brand) en la columna metadata
-SELECT *
-FROM data_types_demo
-WHERE available = TRUE
-  AND metadata ? 'brand';
+SELECT * FROM data_types_demo WHERE metadata ? 'brand';
 
--- 2. Encuentra los artículos lanzados después de una fecha específica (ejemplo: 2025-01-01)
-SELECT *
-FROM data_types_demo
-WHERE launch_date > '2025-07-01';
+-- 2. Encuentra los artículos lanzados después de una fecha específica 
+SELECT * FROM data_types_demo WHERE launch_date>'2025-01-01';
 
-
--- 3. Busca productos que tengan un tag específico (ejemplo: 'portátil')
-SELECT *
-FROM data_types_demo
-WHERE 'tecnología' = ANY(tags);
-
+-- 3. Busca productos que tengan un tag específico 
+SELECT * FROM data_types_demo WHERE 'gratis'=ANY(tags);
 
 -- 4. Muestra IP y dirección MAC de todos los productos
-SELECT name, ip_address, mac_address
-FROM data_types_demo;
+SELECT ip_address, mac_address FROM data_types_demo;
 
-
--- 5. Muestra nombre y coordenadas de todos los productos
-SELECT name, coordinates
-FROM data_types_demo;
+-- 5. Muestra coordenadas de todos los productos
+SELECT coordinates FROM data_types_demo;
 
 
