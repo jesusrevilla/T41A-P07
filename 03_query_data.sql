2581482d07bf629bc373f8784d79483e5d79433a
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-10-08 19:16:23 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,11 @@
 
+SELECT name, metadata FROM data_types_demo WHERE available=TRUE AND metadata ? 'brand'; 
+
+SELECT name, launch_date FROM data_types_demo WHERE launch_date > '2025-10-08';
+
+SELECT name, tags FROM data_types_demo WHERE 'premium' = ANY(tags);
+
+SELECT name, ip_address, mac_address FROM data_types_demo;
+
+
+SELECT name, coordinates FROM data_types_demo;
