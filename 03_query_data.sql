8b6a01881b6ddf46d4b675a1042fa4ea62788a57
David Vidales <vidalesrmzdavid@gmail.com>
2025-10-08 23:21:18 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,24 @@
+SELECT id, name, price, metadata->>'brand' AS brand
+FROM data_types_demo
+WHERE available = TRUE
+  AND metadata ? 'brand';
 
+SELECT id, name, launch_date
+FROM data_types_demo
+WHERE launch_date > '2024-01-01';
+
+SELECT id, name, launch_date
+FROM data_types_demo
+WHERE launch_date > '2024-01-01';
+
+SELECT id, name, tags
+FROM data_types_demo
+WHERE 'oficina' = ANY(tags);
+
+SELECT name, ip_address, mac_address
+FROM data_types_demo;
+
+SELECT name, coordinates, 
+       coordinates[0] AS x, 
+       coordinates[1] AS y
+FROM data_types_demo;
